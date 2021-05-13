17e83337ecc2a56f33fb77aca31128f47935a3e0
Marcos Carrillo <83974878+0373067@users.noreply.github.com>
2021-05-12 21:10:17 -0400

    Update Messages.java

--- LogMessages/src/LogMessage/Messages.java
+++ LogMessages/src/LogMessage/Messages.java
@@ -2,7 +2,7 @@ package LogMessage;
 
 import java.util.ArrayList;
 
-public class Messages {
+public class Messages { // Includes list of messages along with some getters and setters. 
 
     public static ArrayList<String> descriptions = new ArrayList<>();
     public static ArrayList<String> ids = new ArrayList<>();
@@ -22,7 +22,7 @@ public class Messages {
         fullMsg = id + ": " + description;
     }
 
-    private boolean search(String word, ArrayList<String> list) {
+    private boolean search(String word, ArrayList<String> list) { // Search method for any strings within list.
         for(String s: list) {
             if(s.equals(word)) {
                 return false;
@@ -31,27 +31,27 @@ public class Messages {
         return true;
     }
 
-    public String getDes() {
+    public String getDes() { // Getter for description.
         return description;
     }
 
-    public String getId() {
+    public String getId() { // Getter for identification.
         return id;
     }
 
-    public String getMessage() {
+    public String getMessage() { // Getter for message.
         return fullMsg;
     }
 
-    public void setId(String id) {
+    public void setId(String id) { // Setter for identification.
         this.id = id;
     }
 
-    public void setDes(String description) {
+    public void setDes(String description) { // Setter for description.
         this.description = description;
     }
 
-    public void setMsg(String id, String description) {
+    public void setMsg(String id, String description) { // Setter for full message constiting of identification:description.
         this.id = id;
         this.description = description;
         fullMsg = id + ": " + description;
